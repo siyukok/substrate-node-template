c0e1dff59a257851cfe02f106a12f9d4460d09ef
siyukok <devilkeven@outlook.com>
2023-06-02 23:34:20 +0800

    refine

--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -14,42 +14,48 @@ fn create_kitty_works() {
         assert_eq!(KittiesModule::kitties(kitty_id).is_some(), true);
         System::assert_last_event(Event::KittyCreated
         {
-            who: 1,
-            kitty_id: 0,
-            kitty: KittiesModule::kitties(kitty_id).unwrap()
+            who: account_id,
+            kitty_id,
+            kitty: KittiesModule::kitties(kitty_id).unwrap(),
         }.into());
 
         assert_eq!(KittiesModule::kitty_owner(kitty_id), Some(account_id));
         assert_eq!(KittiesModule::kitty_parents(kitty_id), None);
-        crate::NextKittyId::<Test>::set(crate::KittyId::max_value());
+        NextKittyId::<Test>::set(KittyId::MAX);
         assert_noop!(
-            KittiesModule::create(RuntimeOrigin::signed(account_id)), Error::<Test>::InvalidKittyId
-            );
+            KittiesModule::create(RuntimeOrigin::signed(account_id)),
+            Error::<Test>::InvalidKittyId
+        );
     })
 }
 
 #[test]
 fn breed_kitty_works() {
     new_test_ext().execute_with(|| {
+        let kitty_id = 0;
         let account_id = 1;
         assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
         assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
-        assert_eq!(KittiesModule::next_kitty_id(), 2);
-        assert_ok!(KittiesModule::breed(RuntimeOrigin::signed(account_id), 0, 1));
+        assert_eq!(KittiesModule::next_kitty_id(), kitty_id + 1 + 1);
+        assert_ok!(KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id+1));
+
+        let breed_kitty_id = 2;
         System::assert_last_event(Event::KittyBreed
         {
-            who: 1,
-            kitty_id: 2,
-            kitty: KittiesModule::kitties(2).unwrap()
+            who: account_id,
+            kitty_id: breed_kitty_id,
+            kitty: KittiesModule::kitties(breed_kitty_id).unwrap(),
         }.into());
-        assert_eq!(KittiesModule::kitty_owner(2), Some(account_id));
-        assert_eq!(KittiesModule::kitty_parents(2), Some((0, 1)));
+        assert_eq!(KittiesModule::kitty_owner(breed_kitty_id), Some(account_id));
+        assert_eq!(KittiesModule::kitty_parents(breed_kitty_id), Some((kitty_id, kitty_id + 1)));
         assert_noop!(
-            KittiesModule::breed(RuntimeOrigin::signed(account_id), 1, 1), Error::<Test>::SameKittyId
-            );
+            KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id),
+            Error::<Test>::SameKittyId
+        );
         assert_noop!(
-            KittiesModule::breed(RuntimeOrigin::signed(account_id), 1, 9999), Error::<Test>::InvalidKittyId
-            );
+            KittiesModule::breed(RuntimeOrigin::signed(account_id), 1, 9999),
+            Error::<Test>::InvalidKittyId
+        );
     })
 }
 
@@ -66,12 +72,13 @@ fn transfer_kitty_works() {
         System::assert_last_event(Event::KittyTransferred
         {
             from: account_id,
-            kitty_id,
             to: recipient,
+            kitty_id,
         }.into());
         assert_eq!(KittiesModule::kitty_owner(kitty_id), Some(recipient));
         assert_noop!(
-            KittiesModule::transfer(RuntimeOrigin::signed(9999), recipient, kitty_id), Error::<Test>::NotKittyOwner
-            );
+            KittiesModule::transfer(RuntimeOrigin::signed(9999), recipient, kitty_id),
+            Error::<Test>::NotKittyOwner
+        );
     })
 }
